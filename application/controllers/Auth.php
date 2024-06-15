ec2d4f053176c73c47ca79ae50bf1b15e80fa7e1
zheandrizky <zheandrizky@gmail.com>
2024-06-15 15:26:32 +0700

    register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -47,18 +47,36 @@ class Auth extends CI_Controller
 
     }
 
-    public function register($nik, $password)
+    public function register()
     {
-        // Hash the password using bcrypt
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        if ($this->session->userdata('logged_in')) {
+            redirect('dashboard'); // Jika sudah login, arahkan ke dashboard
+        }
 
-        // Insert the user data into the database
-        $data = array(
-            'nik' => $nik,
-            'password_hash' => $hashed_password
-        );
-        $this->db->insert('anggota', $data);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nik = $this->input->post('nik');
+            $password = $this->input->post('password');
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            $data = array(
+                'nik' => $nik,
+                'jabatan' => 'anggota',
+                'password' => $hashed_password
+            );
+
+            if ($this->db->insert('anggota', $data)) {
+                $this->session->set_flashdata('success', 'Akun berhasil dibuat. Silakan login.');
+                redirect('auth/login');
+            } else {
+                $data['error'] = 'Gagal melakukan registrasi. Silakan coba lagi.';
+                $this->load->view('auth/register', $data);
+            }
+        }else{
+            $this->load->view('auth/register');
+        }
+
     }
+
 
 
     public function logout()
